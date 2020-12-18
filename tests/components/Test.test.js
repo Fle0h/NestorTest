e3accf3bfa499f74361a0afc9b9d3d6157e529b5
Fle0h <davidf69220@gmail.com>
2020-12-18 11:29:07 +0100

    last changes

--- tests/components/Test.test.js
+++ tests/components/Test.test.js
@@ -1,24 +1,27 @@
-import { expect } from 'chai';
-import app from 'src/reducers/app';
+import { should } from 'chai';
+import appReducer, { initialState } from 'src/reducers/app';
+import { SAVE_ALL_APARTMENTS, saveAllApartments } from 'src/actions/app';
+
+should();
 
 describe('App Reducer', () => {
-  it('Should handle INCREMENT', () => {
-    const initialState = { count: 0 };
-
-    const newState = app(initialState, { type: 'INCREMENT' });
-
-    expect(newState).to.eql({
-      count: 1,
+  describe('Structure', () => {
+    it('should be a function', () => {
+      appReducer.should.be.a('function');
+    });
+    it('should return initalState', () => {
+      appReducer().should.be.equal(initialState);
     });
   });
 
-  it('Should handle DECREMENT', () => {
-    const initialState = { count: 1 };
-
-    const newState = app(initialState, { type: 'DECREMENT' });
-
-    expect(newState).to.eql({
-      count: 0,
+  describe('Actions', () => {
+    describe(SAVE_ALL_APARTMENTS, () => {
+      it('should change the state', () => {
+        const fakeData = [{ a: 1, b: 1 }, { a: 1, b: 2 }];
+        // on construit l'action avec ces fausses données
+        const action = saveAllApartments(fakeData);
+        appReducer(initialState, action).should.be.eql(`apartments:${[fakeData]}`);
+      });
     });
   });
 });
